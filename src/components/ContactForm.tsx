267fe9fc27909c40d2f83e983141627eee6b3dc9
OB-9 <obamine2006@gmail.com>
2026-05-05 05:17:35 +0100

    🗺️ Update Google Maps iframe with exact location

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -101,7 +101,7 @@ export const ContactForm = () => {
             }}
           >
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12740.!2d9.87!3d37.27!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd1!2sDonatello!5e0!3m2!1sfr!2stn!4v1"
+              src="https://maps.google.com/maps?q=37.2754064,9.877565&z=17&output=embed"
               width="100%"
               height="100%"
               style={{ border: 0 }}
